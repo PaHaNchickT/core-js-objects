ef7e367823521ad7d475040dbcdbd2c0fdfaeaff
Pavel <you@example.com>
2024-02-26 23:53:50 +0300

    feat: implement sellTickets task (26.02.24 23:53)

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -160,8 +160,17 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let storage = 0;
+  queue.forEach((e) => {
+    if (e === 25) {
+      storage += e;
+    } else {
+      storage -= e - 25;
+    }
+  });
+  if (storage < 0) return false;
+  return true;
 }
 
 /**
