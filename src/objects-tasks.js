99e5b782cb56dd3f537b65b134c815862ee1ebff
Pavel <you@example.com>
2024-02-27 00:37:24 +0300

    feat: implement fromJSON task (27.02.24 00:37)

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -222,8 +222,14 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const out = {};
+  Object.setPrototypeOf(out, proto);
+  Object.entries(JSON.parse(json)).forEach((e) => {
+    const [key, val] = e;
+    out[key] = val;
+  });
+  return out;
 }
 
 /**
